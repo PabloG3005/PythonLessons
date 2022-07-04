fd6ff574a618df4fc96b0b2ee9b917312d085feb
PabloG3005 <pawelguz3005@gmail.com>
2022-07-04 23:05:05 +0200

    sums calculator

---
+++ Calculator Game/Calc.py
@@ -0,0 +1,11 @@
+
+
+x = int (input('Podaj pierwsza liczbe: '))
+y = int (input('Podaj druga liczbe: '))
+z = int (input('Suma tych liczb to (wpisz): '))
+
+
+if (x + y == z):
+    print ('Zgadza sie! Suma tych liczb to: ', z)
+else:
+    print ('Nie zgadza sie, sprobuj jeszcze raz')
