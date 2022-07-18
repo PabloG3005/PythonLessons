e36ddd809b61ccdcd028a4463d7c0a6df27ffbaf
Tomasz Guz <microsol66@hotmail.com>
2022-07-18 19:57:38 +0200

    Mnozenie i iloraz

---
+++ Calculator Game/Mulitplication.py
@@ -0,0 +1,11 @@
+
+
+x = int (input('Podaj pierwsza liczbe: '))
+y = int (input('Podaj druga liczbe: '))
+z = int (input('Iloczyn tych liczb to (wpisz): '))
+
+
+if (x * y == z):
+    print ('Zgadza sie! Iloczyn tych liczb to: ', z)
+else:
+    print ('Nie zgadza sie, iloczyn tych liczb to ', x*y, ' sprobuj jeszcze raz')
---
+++ Calculator Game/pawel.py
@@ -0,0 +1,10 @@
+
+x = input('podaj pierwsza liczbe: ')
+AA = int (x)
+B = int (input('podaj druga liczbe: '))
+C = int (input('iloraz tycz liczb to (wpisz): '))
+
+if (AA / B == C):
+    print ('zgadza sie! iloraz tych liczb to: ', C)
+else:
+    print ('Nie zgadzam sie, Iloraz tych liczb to ', AA/B, 'sprobuj jeszcze raz')
